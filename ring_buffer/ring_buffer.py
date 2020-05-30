998268cc73871e0e6946ce6431548f8f0a904982
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-05-30 09:20:56 -0700

    started ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
+import sys
+
+sys.path.append('./doubly_linked_list')
+
+from doubly_linked_list import DoublyLinkedList
+
+# a ring buffer is a type of queue: FIFO
+# stores in fixed size array
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity # This is the size of the buffer
+        self.current_pointer = None # Using this for temp storage
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        
 
     def get(self):
         pass
