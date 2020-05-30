8b75f5367163f901c47a381b2f14f355214244ff
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-05-30 09:23:54 -0700

    added append
    to ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -13,7 +13,19 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        
+        # if buffer is empty, add item to head and set the pointer to head
+        if self.storage.length == 0:
+            self.storage.add_to_head(item)
+            self.current_pointer = self.storage.head
+
+        # when buffer is full and pointer is at head:
+        elif self.storage.length == self.capacity and self.current_pointer is self.storage.head:
+            # remove from head
+            self.storage.remove_from_head()
+            # add item to tail
+            self.storage.add_to_tail(item)
+            # set item at tail as current item
+            self.current_pointer = self.storage.tail
 
     def get(self):
         pass
